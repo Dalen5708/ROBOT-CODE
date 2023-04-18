18fb6748510030827819fe5e43ae4ee61bb67061
Dalen5708 <113061723+Dalen5708@users.noreply.github.com>
2023-04-18 14:34:29 -0500

    Create RobotCode.ino

---
+++ RobotCode.ino
@@ -0,0 +1,102 @@
+const int ENA = 6;
+const int IN1 = 7;
+const int IN2 = 8;
+const int IN3 = 9;
+const int IN4 = 10;
+const int ENB = 11;
+int trig = 2;
+int echo = 3;
+long duration;
+int distance;
+int Rdistance;
+int Ldistance;
+
+void Forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void Stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void Back() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void Right() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void Left() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void setup() {
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  analogWrite(ENA, 115);
+  analogWrite(ENB, 80);
+  
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = duration * 0.034/2;
+  
+  
+  if (distance < 50){
+    Stop();
+    delay(1000);
+    Back();
+    delay(300);
+  }
+  if (distance < 50){
+    Right();
+    delay(300);
+    // distance = Rdistance;
+    // delay(1000);
+  }
+  // if (distance < 50){
+  //   Left();
+  //   delay(300);
+  //   // distance = Ldistance;
+  //   // delay(1000);
+  // }
+  // if (Ldistance > Rdistance){
+  //   Left();
+  //   delay(300);
+  // }
+  //   else{
+  //     Right();
+  //     delay(300);
+  //   }
+  
+  if (distance > 50){
+    Forward();
+  }
+}
